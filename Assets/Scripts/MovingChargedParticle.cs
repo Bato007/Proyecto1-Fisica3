076baf1d226e82c60f97a33c774baeafdfef1cd1
Requetetin <martinamadog@gmail.com>
2020-09-08 14:08:34 -0600

    Simulacion inicial

---
+++ Assets/Scripts/MovingChargedParticle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MovingChargedParticle : ChargedParticle
+{
+    public float mass = 1;
+    public Rigidbody rb;
+
+    public float equis;
+    public float ye;
+
+
+    private void Start()
+    {
+        UpdateColor();
+
+
+        rb = gameObject.AddComponent<Rigidbody>();
+        rb.mass = mass;
+        rb.useGravity = false;
+
+        rb.AddForce(new Vector3(equis,ye,0));
+    }
+
+}
